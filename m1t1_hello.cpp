f1a54880e65af3a43b5ec96945dbadcd380cb00c
Alphons Shaji <shajia5107@student.faytechcc.edu>
2025-08-20 15:37:26 +0000

    Like games

---
+++ m1t1_hello.cpp
@@ -0,0 +1,16 @@
+/*
+CSC 134
+M1T1 - Hello World
+Alphons Shaji
+8/20/25
+First program
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl; 
+    cout << "My foo is games" << endl;
+    return 0;
+}
