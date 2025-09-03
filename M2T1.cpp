050baae3b405891a26bc0cdbfda90d64b50ae6c0
Alphons Shaji <shajia5107@student.faytechcc.edu>
2025-09-03 15:17:23 +0000

    real

--- M2T1.cpp
+++ M2T1.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-    string_name = "mangoes";
-    double cost_per= 0.67
-    int amount= 99
+    string string_name = "mangoes";
+    double cost_per= 0.67;
+    int amount= 99;
     int amount_purchased;
     double total_cost;
 
@@ -12,12 +12,12 @@ int main() {
     cout<< "greetings1 welcome to our " << string_name<< " store1"<< endl;
     //ask info
     cout<< " each "<< string_name << " cost $" << cost_per<< endl;
-    cout<< "we have " << amount<< " in stock " >> endl;
+    cout<< "we have " << amount<< " in stock " << endl;
     cout<< " how many would you like to buy?"<< endl; 
-    cin>> amount_purchased
-
+    cin>> amount_purchased;
     // calculation
     total_cost=amount_purchased* cost_per;
 
     cout<<""<< endl;
     cout<< amount_purchased<< " mangoes cost $"<< total_cost<< endl;
+}
