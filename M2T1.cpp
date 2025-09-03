1207955bd6cbb33dc3271c37e9573c9b2dbd6f75
Alphons Shaji <shajia5107@student.faytechcc.edu>
2025-09-03 15:12:16 +0000

    first code

---
+++ M2T1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string_name = "mangoes";
+    double cost_per= 0.67
+    int amount= 99
+    int amount_purchased;
+    double total_cost;
+
+    // greet user
+    cout<< "greetings1 welcome to our " << string_name<< " store1"<< endl;
+    //ask info
+    cout<< " each "<< string_name << " cost $" << cost_per<< endl;
+    cout<< "we have " << amount<< " in stock " >> endl;
+    cout<< " how many would you like to buy?"<< endl; 
+    cin>> amount_purchased
+
+    // calculation
+    total_cost=amount_purchased* cost_per;
+
+    cout<<""<< endl;
+    cout<< amount_purchased<< " mangoes cost $"<< total_cost<< endl;
