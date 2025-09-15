35d60ec6b415fd84b3750560af4ccf94430fb136
Alphons Shaji <shajia5107@student.faytechcc.edu>
2025-09-15 14:48:16 +0000

    started

---
+++ m2hw1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void question1() {
+    cout << "question1" << endl;
+}
+
+void question2() {
+    cout<< "question 2" << endl;
+}
+
+int main() {
+    // call function
+    question1();
+    question2();
+
+    cout<< "example of pirnintg out correct money values"
+}
