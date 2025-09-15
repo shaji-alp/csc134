43b4dda96c4dce3fea55fff81a4ad23fabd24375
Alphons Shaji <shajia5107@student.faytechcc.edu>
2025-09-15 15:26:55 +0000

    done

--- m1hw1.cpp
+++ m1hw1.cpp
@@ -15,4 +15,10 @@ int main() {
     int  release_year = 2001;
     double box_income = 492.5;
     cout << "the movie" << movie_name << " came out in " << release_year << ", and had a gross of around " << box_income << " Million dollars" << endl;
+    cout<< "quotes"<< endl;
+    cout<< "“Ogres are like onions.”"<< endl;
+    cout<< "“Hey! I’m no one’s messenger boy, all right? I’m a delivery boy.”"<< endl;
+    cout<< "“What are you doing in my swamp!?”"<< endl;
+    cout<< "“That’ll do, Donkey. That’ll do.”"<< endl;
+    cout<< "“All right, you’re going the right way for a smack bottom.”"<< endl;
 } 
