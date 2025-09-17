7c97c5c7162b87e0a4007d0d6d896c00d9f29f10
Alphons Shaji <shajia5107@student.faytechcc.edu>
2025-09-17 15:33:03 +0000

    done

--- M2HW1.cpp
+++ M2HW1.cpp
@@ -26,9 +26,77 @@ int main() {
     cout<<"Balance; "<< balance<< endl;
     /*End of Question1*/
 
+    cout<<endl;
+    cout<<endl;
+
     /*Question2 */
-    
+    double length, width, height,volume;
+    double cost_to_make, charge_to_customer;
+    double profit;
 
 
+    const double COST_PER_CUBIC_FOOT= 0.3;
+    const double CHARGE_PER_CUBIC_FOOT= 0.52;
+
+    cout<< "welcome to thr crate program "<< endl;
+    cout<< "crate lenghts ";
+    cin>> length;
+    cout<< "crate width ";
+    cin>> width;
+    cout<< "crate height ";
+    cin>> height;
+
+    volume= length*height* width;
+    cost_to_make= volume* COST_PER_CUBIC_FOOT;
+    charge_to_customer= volume* CHARGE_PER_CUBIC_FOOT;
+    profit= charge_to_customer- cost_to_make;
+
+    cout<<endl;
+    cout<<"your crate is "<< volume<< " cubic feet"<< endl;
+    cout<< "customer price: $"<< charge_to_customer<< endl;
+    cout<< "cost to build $"<< cost_to_make << endl;
+    cout<< "profit $"<<profit << endl;
+    /*End of Q2*/
+
+    cout<<endl;
+    cout<<endl;
+
+    /*Question 3*/
+    const int slice_person=3;
+    int slice_pizza, box, people, leftovers;
+
+    cout<<"Each visitor gets 3 slices"<< endl;
+    cout<<"enter the total amount of pizzas: " ;
+    cin>> box;
+    cout<<"Slices per pizza: ";
+    cin>>slice_pizza;
+    cout<<"Number of visitors: ";
+    cin>> people;
+
+    leftovers=(box*slice_pizza)-(slice_person*people);
+    cout<< "there are "<< leftovers << " slices of pizza leftover"<< endl;
+    /*end of question 3*/
+
+    cout<<endl;
+    cout<<endl;
+
+    /*Question 4*/
+    string school, team, cheer, cheer1, cheer2, schoolcheer,teamcheer;
+    school= "FTCC ";
+    team= "Trojans ";
+    cheer1= "Let's ";
+    cheer2= "go ";
+    cheer = cheer1 + cheer2;
+    schoolcheer = cheer+ school;
+    teamcheer= cheer+ team;
+
+    cout<<schoolcheer<<endl;
+    cout<<schoolcheer<<endl;
+    cout<<schoolcheer<<endl;
+    cout<<teamcheer<<endl;
+    /*End of question 4*/
+
+
+    return 0
 
 }
