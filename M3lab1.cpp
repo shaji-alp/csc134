dfb606096bd80400848d131ee84967b7c786b3c2
Alphons Shaji <shajia5107@student.faytechcc.edu>
2025-09-24 15:46:44 +0000

    done

--- M3lab1.cpp
+++ M3lab1.cpp
@@ -9,6 +9,7 @@ using namespace std;
 void house();
 void mcdonald();
 void chickfila();
+int choice;
 
 
 int main(){
@@ -46,4 +47,43 @@ void house(){
         cout<<"you cant sleep because you just woke up"<< endl;
         house();
     }
+}
+void mcdonald(){
+    cout<<"You came to McDonalds"<< endl;
+    cout<<"1. Go to cashier"<< endl;
+    cout<< "2.Go to evil cashier" << endl;
+
+    cin>> choice;
+
+    if (choice==1){
+        cout<<"Cashier- Would you like to buy cheeseburger"<< endl;
+        cout<<"1.yea"<< endl;
+        cout<<"2.no"<< endl;
+        cin>> choice;
+
+        if (choice==1){
+            cout<<"You bought burger"<<endl;
+            cout<<"You are not hungry anymore"<<endl;
+            cout<<"You went back home and slept"<<endl;
+        }
+        else if(choice==2){
+            cout<<"ok"<<endl;
+            mcdonald();
+        }
+    }
+    else if(choice==2){
+        cout<< "greetings mortal ! eat this evil cheeseburger"<< endl;
+        cout<< "1.yes"<< endl;
+        cout<< "2.Yes"<< endl;
+        cout<< " You ate the evil cheeseburger"<< endl;
+        cout<< " You are evil now"<< endl;
+        cout<< " You will never be hungry again"<< endl;
+    }
+}
+void chickfila(){
+    cout<<"ChicFilA is closed because it sunday"<< endl;
+    cout<<"You are very hungry"<< endl;
+    cout<<"you died of starvation"<< endl;
+
+
 }
