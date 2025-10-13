972c33d98fe767611e727578049da78cbb2a0e33
Alphons Shaji <shajia5107@student.faytechcc.edu>
2025-10-13 14:49:50 +0000

    eaeaeadea

---
+++ M3HW1.cpp
@@ -0,0 +1,15 @@
+// CSC 134
+// M3HW1 - Gold
+// Alphons Shaji
+// 10/13/25
+
+#include <iostream>
+using namespace std;
+void ques1();
+void ques2();
+void ques3();
+void ques4();
+
+int main(){
+    cout<<"question 1"<<endl;
+}
