13bfa39571e4ff61fb74ebff5a967056700ce806
Alphons Shaji <shajia5107@student.faytechcc.edu>
2025-11-03 16:25:28 +0000

    choice

--- M5HW.cpp
+++ M5HW.cpp
@@ -1,2 +1,44 @@
 #include <iostream>
 using namespace std;
+
+void averagerainfall();
+void volume();
+void romannum();
+void geometrycalc();
+void distance();
+
+int main(){
+    int choice;
+    cout<<"What function to run?"<<endl;
+    cout<<"1. Average rainfall"<<endl;
+    cout<<"2. Volume"<<endl;
+    cout<<"3. Roman numerals"<<endl;
+    cout<<"4. Geometry calculator"<<endl;
+    cout<<"5. Distance calculator"<<endl;
+    cout<<"enter choice(1-5) ";
+    cin>>choice;
+
+    if (choice==1){
+        void averagerainfall();
+    }
+    else if (choice==1){
+        void volume();
+    }
+    else if (choice==1){
+        void romannum();
+    }
+    else if (choice==1){
+        void geometrycalc();
+    }
+    else if (choice==1){
+        void distance();
+    }
+    else{
+        cin.clear();
+        cout<<"Invalid input"<<endl;
+        main();
+
+    }
+    
+    
+}
