4caa9ce427aef7b6b8cf5a8f7a04bf990e0a8eb3
Alphons Shaji <shajia5107@student.faytechcc.edu>
2025-11-26 16:38:06 +0000

    aa

--- PROJECT.cpp
+++ PROJECT.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 int Xenter();
 int Oenter();
-void validinput();
-bool checkwin();
+bool validinput(int loc);
+bool checkwin(string x);
 void board();
 
 int main(){
@@ -18,34 +18,44 @@ void board(){
     int hi=0;
     const int spot=9;
     string placeholder[spot]={"1","2","3","4","5","6","7","8","9"};
-    while (win==false){
-        win=checkwin();
-        cout<<placeholder[1]<<"\t||\t"<<placeholder[2]<<"\t||\t"<<placeholder[]"\n";
-        cout<<"----------------------------------\n";
-        cout<<"X\t||\tX\t||\tX\n";
-        cout<<"----------------------------------\n";
-        cout<<"X\t||\tX\t||\tX\n";
-        win=true;
+    for (int i=0;i<10;i++){
+        cout<<placeholder[0]<<"\t||\t"<<placeholder[1]<<"\t||\t"<<placeholder[2]<<"\n";
+        cout<<"__________________________________\n";
+        cout<<placeholder[3]<<"\t||\t"<<placeholder[4]<<"\t||\t"<<placeholder[5]<<"\n";
+        cout<<"__________________________________\n";
+        cout<<placeholder[6]<<"\t||\t"<<placeholder[7]<<"\t||\t"<<placeholder[8]<<"\n";
+        win=false;
         if (hi==0){
             hi=1;
-            Xenter();
+            int xnum;
+            xnum=Xenter();
+            placeholder[xnum]="❌";
         }
         else if (hi==1){
             hi=0;
-            Oenter();
+            int onum;
+            onum=Oenter();
+            placeholder[onum]="🟢";
         }
       
+    }
+    if (win==false){
+        cout<<"Tie"<<endl;
     }
 }
 int Xenter(){
     int Xloc;
+    bool cont=false;
     cout<<"Player 1 enter location of X"<<endl;
     cin>>Xloc;
+    Xloc=Xloc-1;
     return Xloc;
 }
 int Oenter(){
     int Oloc;
+    bool cont=false;
     cout<<"Player 2 enter location of O"<<endl;
     cin>>Oloc;
+    Oloc=Oloc-1;
     return Oloc;
 }
