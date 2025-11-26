9fe3b487e7944f2f3aeafd8b4b9775736368bd79
Alphons Shaji <shajia5107@student.faytechcc.edu>
2025-11-26 05:14:34 +0000

    aa

--- PROJECT.cpp
+++ PROJECT.cpp
@@ -2,3 +2,23 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+
+using namespace std;
+void player1();
+void player2();
+void XorO();
+void validinput();
+void checkwin();
+void board();
+
+int main(){
+    cout<<"Welcome. Player 1 is X. Player 2 is O"<<endl;
+    board();
+}
+void board(){
+    bool win= false;
+    while (win==false){
+        cout<<R"(uyybv8y
+            iyhiyvb)";
+    }
+}
